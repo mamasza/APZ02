4e6cfe0fb94dff47eb140fb3312982b7060ca2f5
mamasza <107420170+mamasza@users.noreply.github.com>
2022-06-27 21:21:44 +0200

    first commit

---
+++ cw1/script.js
@@ -0,0 +1,11 @@
+const zdjecia = ["", "Ewa", "Maria"];  // tablica (array) stringów 
+	const views = [ 		// tablica obiektów
+      {
+        "src" : "1.jpg",
+        "name" : " Pierwszy "
+      },
+      {
+        "src" : "2.jpg",
+        "name" : " Drugi "
+      }
+    ];
