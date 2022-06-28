3cd508cb8ad34a14110686ef28a8b7227bb1b0a1
mamasza <107420170+mamasza@users.noreply.github.com>
2022-06-28 19:41:39 +0200

    19_40

--- cw1/script.js
+++ cw1/script.js
@@ -36,24 +36,11 @@ function changeHTMLInfo (slideNr){
   const title = data[slideNr].title 
   const titleDOM =document.querySelector("h2") 
   titleDOM.innerHTML=title
-
-  console.log(data[slideNr].title,data[slideNr].src);
-  const picture = document.querySelector('.picture')
-  picture.src = data[slideNr].src;
+  const image = data[slideNr].src
+  const imageDOM = document.querySelector("img")
+  imageDOM.src="img/"+image
+  image.src = data[slideNr].src;
   const h2 = document.querySelector('h2')
   h2.innerText = data[slideNr].title;
 
-}
-const lewa = document.querySelector('.lefftArr')
-lewa.onclick = function(){
-    console.log("click")
-    slideNr = slideNr-1;
-    changeHTMLinfo(slideNr);
-}
-
-const prawa = document.querySelector('.rightArr')
-prawa.onclick = function(){
-    console.log("click");
-    slideNr = slideNr+1;
-    changeHTMLinfo(slideNr);
 }
